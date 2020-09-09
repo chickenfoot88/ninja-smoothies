062ed088a2989f512b00b25c3cc7a6dfd5f22b82
aismagilov <aismagilov@c-i-p.ru>
2020-09-09 13:40:54 +0300

    add TheNavbar component

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,17 @@
 <template>
   <div id="app">
+    <the-navbar></the-navbar>
   </div>
 </template>
 
 <script>
+import TheNavbar from '@/components/TheNavbar'
 
 export default {
   name: 'App',
+  components: {
+    TheNavbar
+  }
 }
 </script>
 
